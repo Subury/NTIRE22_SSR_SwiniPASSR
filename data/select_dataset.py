b9293ab56c9794d1498c493da653288064e146dc
subury <weizeqiang@bupt.edu.cn>
2022-02-17 04:46:03 +0000

    Time: 2022-02-17 Abstract: Initialize the code repository for the CVPR2022 Workshop

---
+++ data/select_dataset.py
@@ -0,0 +1,26 @@
+
+
+'''
+# --------------------------------------------
+# select dataset
+# --------------------------------------------
+# Kai Zhang (github: https://github.com/cszn)
+# --------------------------------------------
+'''
+
+
+def define_Dataset(dataset_opt):
+    dataset_type = dataset_opt['dataset_type'].lower()
+
+
+    # -----------------------------------------
+    # super-resolution
+    # -----------------------------------------
+    if dataset_type in ['sr', 'super-resolution']:
+        from data.dataset_sr import DatasetSR as D
+    else:
+        raise NotImplementedError('Dataset [{:s}] is not found.'.format(dataset_type))
+
+    dataset = D(dataset_opt)
+    print('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__, dataset_opt['name']))
+    return dataset
